28201712401d83a0019df0741da31582b6d524ba
Maksim <72063751+psevdocoder@users.noreply.github.com>
2024-11-28 19:36:01 +0300

    feat: config ref

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"os"
 
 	"gopkg.in/yaml.v3"
@@ -8,15 +9,21 @@ import (
 
 // Config struct of app's config
 type Config struct {
-	App struct {
-		Mock      bool   `yaml:"mock"`
-		LogLevel  string `yaml:"log_level"`
-		LogFormat string `yaml:"log_format"`
+	App App `yaml:"app"`
+}
 
-		HTTP struct {
-			ListenAddr string `yaml:"listen_addr"`
-		} `yaml:"http"`
-	} `yaml:"app"`
+type App struct {
+	Mock      bool   `yaml:"mock"`
+	LogLevel  string `yaml:"log_level"`
+	LogFormat string `yaml:"log_format"`
+
+	Test string `yaml:"test"`
+
+	HTTP HTTP `yaml:"http"`
+}
+
+type HTTP struct {
+	ListenAddr string `yaml:"listen_addr"`
 }
 
 // MustLoad load config from file or panic if error
@@ -30,6 +37,10 @@ func MustLoad(configPath string) *Config {
 	if err = yaml.Unmarshal(data, &config); err != nil {
 		panic(err)
 	}
+
+	fmt.Println(config.App.Test)
+
+	fmt.Printf("config: %+v\n", config)
 
 	return &config
 }
