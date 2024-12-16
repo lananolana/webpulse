2ca6b7dcaa43b44bfc16c307300cbd666eab1b88
Lana Alekseeva <lanaalekseevaa@gmail.com>
2024-12-16 17:07:41 +0000

    test: add tests for ssl & domain check

--- backend/tests/integration/main_test.go
+++ backend/tests/integration/main_test.go
@@ -4,18 +4,26 @@ import (
 	"os"
 	"testing"
 
+	"github.com/lananolana/webpulse/backend/internal/config"
 	"github.com/lananolana/webpulse/backend/tests/integration/client"
 	"github.com/lananolana/webpulse/backend/tests/integration/step"
 )
 
+const (
+	localhost  = "http://localhost:8080"
+	configPath = "../../configs/app.yaml"
+)
+
 var (
 	c *client.Client
-	s *step.Steps
+	s step.DomainChecks
 )
 
 func TestMain(t *testing.M) {
 
-	c = client.NewClient("http://localhost:8080")
+	config := config.MustLoad(configPath)
+
+	c = client.NewClient(&config.App.HTTPServer)
 	s = step.NewSteps(c)
 
 	run := t.Run()
--- backend/tests/integration/testdata/testdata.go
+++ backend/tests/integration/testdata/testdata.go
@@ -1,6 +1,10 @@
 package testdata
 
-var URLs = []string{
+const (
+	FailMessage = "Invalid domain name format"
+)
+
+var SecureURLs = []string{
 	"https://google.com",
 	"https://youtube.com",
 	"https://facebook.com",
@@ -20,4 +24,16 @@ var URLs = []string{
 	"aliexpress.ru",
 	"https://apple.com",
 	"gazeta.ru",
+}
+
+var UnsecureURLs = []string{
+	"http://google.com",
+	"http://httpstat.us",
+	"http://neverssl.com",
+	"http://httpforever.com",
+}
+
+var WrongURLs = []string{
+	"htt:/google.com",
+	"google",
 }
